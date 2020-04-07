b7665275ef709570ca3af7fb0eb7436f9f78fb04
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-07 22:30:34 +0300

    Repository Pattern Part 4 : CategoryRepository implementation

--- Eshop/Eshop.DataAccess/Data/ApplicationDbContext.cs
+++ Eshop/Eshop.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Eshop.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,8 @@ namespace Eshop.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
--- Eshop/Eshop.DataAccess/Data/Repository/CategoryRepository.cs
+++ Eshop/Eshop.DataAccess/Data/Repository/CategoryRepository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using Eshop.DataAccess.Data.Repository.IRepository;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Eshop.DataAccess.Data.Repository
 {
-    class CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryForDropDown()
+        {
+            return _context.Categories.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _context.Categories.FirstOrDefault(s => s.Id == category.Id);
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+
+            _context.SaveChanges();
+        }
     }
 }
