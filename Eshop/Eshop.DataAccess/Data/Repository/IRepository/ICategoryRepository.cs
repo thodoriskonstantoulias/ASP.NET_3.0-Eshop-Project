433ddb97d36636f94bbc513ad3e3fb033fbe7bc7
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-07 21:29:51 +0300

    Repository Pattern Part 1 : IRepository implementation

---
+++ Eshop/Eshop.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.DataAccess.Data.Repository.IRepository
+{
+    interface ICategoryRepository
+    {
+    }
+}
---
+++ Eshop/Eshop.DataAccess/Data/Repository/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.DataAccess.Data.Repository
+{
+    class Repository
+    {
+    }
+}
