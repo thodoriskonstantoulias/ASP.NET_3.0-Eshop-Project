5525959a06c126f629d42b75528b91f0fc997786
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-08 18:38:25 +0300

    Created Category APIs

--- Eshop/Eshop/Areas/Admin/Controllers/CategoryController.cs
+++ Eshop/Eshop/Areas/Admin/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eshop.DataAccess.Data.Repository;
 using Eshop.DataAccess.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,35 @@ namespace Eshop.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
         }
+
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Json(new { data = _unitOfWork.Category.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Category deleted" });
+        }
+
+        #endregion
     }
 }
