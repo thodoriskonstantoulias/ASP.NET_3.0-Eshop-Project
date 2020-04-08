35453628feddae32b07541e2df8ef3ad284bc64a
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-08 22:34:23 +0300

    Upsert completed functionality

--- Eshop/Eshop/Areas/Admin/Controllers/CategoryController.cs
+++ Eshop/Eshop/Areas/Admin/Controllers/CategoryController.cs
@@ -41,6 +41,28 @@ namespace Eshop.Areas.Admin.Controllers
             return View(category);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id  == 0) //Insert
+                {
+                    _unitOfWork.Category.Add(category);                 
+                }
+                else // Update
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
 
 
         #region API CALLS
