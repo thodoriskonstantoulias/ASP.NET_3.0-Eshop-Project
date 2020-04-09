083ce78fa4d680dc866cff1040208d8ee04c326f
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-09 19:23:54 +0300

    Created frequency controller with views

---
+++ Eshop/Eshop/Areas/Admin/Controllers/FrequencyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FrequencyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FrequencyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {       
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAllFrequencies()
+        {
+            return Json(new { data = _unitOfWork.Frequency.GetAll() });        
+        }
+        #endregion
+    }
+}
---
+++ Eshop/Eshop/wwwroot/js/frequency.js
@@ -0,0 +1,67 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+	loadDataTable();
+});
+
+function loadDataTable() {
+	dataTable = $('#tblData').DataTable({
+		"ajax": {
+			"url": "/admin/frequency/GetAllFrequencies",
+			"type": "GET",
+			"datatype": "json"
+		},
+		"columns": [
+			{ "data": "name", "width": "50%" },
+			{ "data": "frequencyCount", "width": "20%" },
+			{
+				"data": "id",
+				"render": function (data) {
+					return `
+                <div class="text-center">
+                    <a href="/admin/frequency/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                        <i class="fas fa-edit"></i> Edit
+                    </a>
+                    &nbsp;
+                    <a onclick= Delete("/admin/frequency/DeleteFrequency/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                        <i class="fas fa-trash-alt"></i> Delete
+                    </a>
+                </div>
+                `;
+				}, "width": "30%"
+			}
+		],
+		"language": {
+			"emptyTable": "No records found."
+		},
+		"width": "100%"
+	})
+}
+
+function Delete(url) {
+	swal({
+		title: "Are you sure you want to delete",
+		text: "You will permanently delete this",
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#DD6B55",
+		confirmButtonText: "Yes delete it",
+		closeOnConfirm: true
+	}, function () {
+		$.ajax({
+			url: url,
+			type: "DELETE",
+			success: function (data) {
+				if (data.success) {
+					toastr.success(data.message);
+					dataTable.ajax.reload();
+				}
+				else {
+					toastr.error(data.message);
+				}
+			}
+		});
+	});
+}
+
+
