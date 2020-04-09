7a46044a8a9503bfdbdb94ad37ca73e3e852b912
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-09 20:50:30 +0300

    Finished Frequency controller

--- Eshop/Eshop/Areas/Admin/Controllers/FrequencyController.cs
+++ Eshop/Eshop/Areas/Admin/Controllers/FrequencyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Eshop.DataAccess.Data.Repository.IRepository;
+using Eshop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eshop.Areas.Admin.Controllers
@@ -22,12 +23,65 @@ namespace Eshop.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Frequency frequency = new Frequency();
+            if (id == null) // Insert
+            {
+                return View(frequency);
+            }
+
+            frequency = _unitOfWork.Frequency.Get(id.GetValueOrDefault());
+            if (frequency == null)
+            {
+                return NotFound();
+            }
+
+            return View(frequency);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Frequency frequency)
+        {
+            if (ModelState.IsValid)
+            {
+                if (frequency.Id == 0) //Insert
+                {
+                    _unitOfWork.Frequency.Add(frequency);
+                }
+                else // Update
+                {
+                    _unitOfWork.Frequency.Update(frequency);
+                }
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(frequency);
+        }
 
         #region API CALLS
         [HttpGet]
         public IActionResult GetAllFrequencies()
         {
             return Json(new { data = _unitOfWork.Frequency.GetAll() });        
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFrequency(int id)
+        {
+            Frequency freqToDelete = _unitOfWork.Frequency.Get(id); 
+            if (freqToDelete == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Frequency.Remove(freqToDelete);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Category deleted" });
         }
         #endregion
     }
