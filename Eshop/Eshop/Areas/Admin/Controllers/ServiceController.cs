b6a789a8d3cad93cf48bc15761b0a516caf0703d
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-09 22:19:30 +0300

    Created Service controller Part 1

---
+++ Eshop/Eshop/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API calls
+        [HttpGet]
+        public IActionResult GetAllServices()
+        {
+            //Check to see if it wants capitals
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties:"category,frequency") });
+        }
+        #endregion
+    }
+}
