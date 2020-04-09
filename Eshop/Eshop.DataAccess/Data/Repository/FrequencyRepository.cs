b6a91ab8bf9e01786054c2a8fa46103e3f1fe8a6
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-09 19:02:25 +0300

    Created Frequency repository

---
+++ Eshop/Eshop.DataAccess/Data/Repository/FrequencyRepository.cs
@@ -0,0 +1,38 @@
+﻿using Eshop.DataAccess.Data.Repository.IRepository;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eshop.DataAccess.Data.Repository
+{
+    public class FrequencyRepository : Repository<Frequency>, IFrequencyRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FrequencyRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetFrequencyForDropDown()
+        {
+            return _context.Frequencies.Select(f => new SelectListItem
+            {
+                Text = f.Name,
+                Value = f.Id.ToString()
+            });
+        }
+
+        public void Update(Frequency frequency)
+        {
+            Frequency getFreq = _context.Frequencies.FirstOrDefault(f => f.Id == frequency.Id);
+            getFreq.Name = frequency.Name;
+            getFreq.FrequencyCount = frequency.FrequencyCount;
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Eshop/Eshop.DataAccess/Data/Repository/IRepository/IFrequencyRepository.cs
@@ -0,0 +1,14 @@
+﻿using Eshop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.DataAccess.Data.Repository.IRepository
+{
+    public interface IFrequencyRepository : IRepository<Frequency>
+    {
+        void Update(Frequency frequency);
+        IEnumerable<SelectListItem> GetFrequencyForDropDown();
+    }
+}
