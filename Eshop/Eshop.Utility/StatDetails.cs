4b0dc29aeeaec2b9d6dce9cff605ba4943ba1b62
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-10 23:40:24 +0300

    Add to cart Action functionality with sessions

---
+++ Eshop/Eshop.Utility/StatDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Utility
+{
+    public static class StatDetails
+    {
+        public const string SessionCart = "Cart";
+    }
+}
--- Eshop/Eshop/Areas/Customer/Controllers/HomeController.cs
+++ Eshop/Eshop/Areas/Customer/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Logging;
 using Eshop.Models;
 using Eshop.DataAccess.Data.Repository.IRepository;
 using Eshop.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Eshop.Utility;
+using Eshop.Extensions;
 
 namespace Eshop.Controllers
 {
@@ -37,6 +40,29 @@ namespace Eshop.Controllers
             var service = _unitOfWork.Service.GetFirstOrDefault(includeProperties:"Category,Frequency", filter:c=> c.Id == id);
 
             return View(service); 
+        }
+
+        public IActionResult AddToCart(int serviceId)
+        {
+            List<int> sessionList = new List<int>(); 
+
+            //Check if there is a session
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(StatDetails.SessionCart)))
+            {
+                sessionList.Add(serviceId);
+                HttpContext.Session.SetObject(StatDetails.SessionCart, sessionList);
+            }
+            else
+            {
+                sessionList = HttpContext.Session.GetObject<List<int>>(StatDetails.SessionCart);
+                if (!sessionList.Contains(serviceId))
+                {
+                    sessionList.Add(serviceId);
+                    HttpContext.Session.SetObject(StatDetails.SessionCart, sessionList);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
