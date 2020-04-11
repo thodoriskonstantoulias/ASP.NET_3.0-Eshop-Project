329cbb3fefef749a77870b9df3c831825bc2c752
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-11 16:36:31 +0300

    Created Index for Cart controller

--- Eshop/Eshop/Areas/Customer/Controllers/CartController.cs
+++ Eshop/Eshop/Areas/Customer/Controllers/CartController.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eshop.DataAccess.Data.Repository.IRepository;
+using Eshop.Extensions;
+using Eshop.Models;
+using Eshop.Models.ViewModels;
+using Eshop.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eshop.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+        CartVM cartVm;
+        public CartController(IUnitOfWork unitOfWork)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+            cartVm = new CartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ServiceList = new List<Service>()
+            };
+
+        }
+
+        public IActionResult Index()
+        {           
+            //Get the services from the session
+            if (HttpContext.Session.GetObject<List<int>>(StatDetails.SessionCart) != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>(StatDetails.SessionCart);
+                foreach (int serviceId in sessionList)
+                {
+                    cartVm.ServiceList.Add(_unitOfWork.Service.GetFirstOrDefault(s => s.Id == serviceId, "Frequency,Category"));
+                }
+            }
+
+            return View(cartVm);
         }
     }
 }
