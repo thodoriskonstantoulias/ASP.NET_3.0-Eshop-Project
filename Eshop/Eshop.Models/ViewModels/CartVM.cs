24289201c940bb3feacc1603413c05bbd7cdbcc6
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-11 16:02:02 +0300

    Created Cart Controller base

---
+++ Eshop/Eshop.Models/ViewModels/CartVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Models.ViewModels
+{
+    public class CartVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IList<Service> ServiceList { get; set; }
+    }
+}
