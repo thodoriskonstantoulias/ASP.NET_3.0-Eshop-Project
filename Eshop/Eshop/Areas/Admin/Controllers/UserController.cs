90b9da96b917dc6cd508795e116d9c4d0742dcd2
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-12 17:08:42 +0300

    Created Users controller

---
+++ Eshop/Eshop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Eshop.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            //Find all users except the one logged in 
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return View(_unitOfWork.User.GetAll(u => u.Id != claims.Value));
+        }
+    }
+}
